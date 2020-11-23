d5749f50d7c515404b92c1a84b3a08f986569557
Pedro Souza <pedro.zelada.souza@gmail.com>
2020-11-23 21:34:15 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+var path = require("path");
+
+app.get('/', (req, res) => {
+
+  res.sendFile(path.join(__dirname, '/views/home.html'));
+
+});
+
+app.listen(3000, () => {
+  console.log('Server started. Listening on port 3000');
+});
