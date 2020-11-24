226c45fff6d6b2067a94f3a4d577bd825558d3d8
Pedro Zelada Souza <pedro.zelada.souza@gmail.com>
2020-11-24 12:53:29 +0000

    Added handlebars & dynamic projects to the home page

--- index.js
+++ index.js
@@ -5,10 +5,31 @@ app.use(express.static('public'));
 
 var path = require("path");
 
+const exphbs = require('express-handlebars');
 
+app.engine('.hbs', exphbs({ extname: '.hbs', defaultLayout: 'main' }));
+app.set('view engine', '.hbs');
+
+/*
 app.get('/', (req, res) => {
 
   res.sendFile(path.join(__dirname, '/views/home.html'));
+
+});
+*/
+
+app.get('/', (req, res) => {
+
+	var someData = [
+		{ name: 'Project A', type: 'Website'},
+		{ name: 'Project B', type: 'C++ App'},
+		{ name: 'Project C', type: 'Java Application'},
+		{ name: 'Project D', type: 'Mobile Game'},
+    ];
+    
+    res.render('home', {
+        data: someData
+    });	
 
 });
 
